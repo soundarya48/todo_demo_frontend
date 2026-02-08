df029c580875f33cec6ebd81fb4705cc55b9b040
soundarya48 <112606471+soundarya48@users.noreply.github.com>
2026-02-08 22:29:21 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/todos`,
+  baseURL: "https://todo-demo-backend-oyhf.onrender.com/api/todos",
 });
 
 export default api;
