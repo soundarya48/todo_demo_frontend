b30a1db9cf88b44bc75af2dc4f0e5ceadac8cb50
soundarya48 <112606471+soundarya48@users.noreply.github.com>
2026-02-08 22:03:57 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/todos`,
+const api = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/todos`,
 });
+
+export default api;
